5359fe05e61be5c7f534eb6c3062e540ebf9aabf
Xi Cephei <col.walter.e.kurtz@gmail.com>
2017-11-13 03:21:11 +0100

    first implementation of hexify() and unhexify()

--- main.cpp
+++ main.cpp
@@ -39,16 +39,166 @@
 // #include <string>
 // #include <sstream>
 // #include <fstream>
-// #include <iostream>
-// #include <iomanip>
+#include <iostream>
+#include <iomanip>
 #include "message.h"
 
 
 // -----------------------------------------------------------------------------
 // Used namespaces                                               Used namespaces
 // -----------------------------------------------------------------------------
-// using namespace std;
+using namespace std;
 
+
+// ------
+// hexify
+// ------
+/**
+ * This function reads each byte from stdin and
+ * writes its hexadecimal representation to stdout.
+ *
+ * @param max  the maximum number of bytes to print per line
+ *             - each byte causes two characters of output
+ *             - max == 0 never breaks a line
+ *
+ * @return
+ * Value | Meaning
+ * ----: | :------
+ *  true | cin reached eof
+ * false | cin didn't reach eof
+ */
+bool hexify(int max = 0)
+{
+  // one byte from the stream
+  char c;
+
+  // initialize byte counter
+  int out = max;
+
+  // read each byte individually from the stream
+  while ( cin.get(c) )
+  {
+    // convert char to unsigned
+    unsigned u = static_cast<unsigned>(c);
+
+    // check if limit is given
+    if (max > 0)
+    {
+      // check if line break is needed
+      if (out == 0)
+      {
+        // start new line (use LF character only)
+        cout << static_cast<char>(10);
+
+        // reset counter
+        out = max;
+      }
+
+      // decrease counter
+      out -= 1;
+    }
+
+    // use iomanip to create hex numbers
+    cout << setfill('0') << hex << uppercase << setw(2) << u;
+  }
+
+  // check if all data has been read
+  return cin.eof();
+}
+
+// --------
+// unhexify
+// --------
+/**
+ * This function reads hex numbers from stdin and
+ * prints their real byte value to stdout.
+ */
+bool unhexify()
+{
+  // first hexadecimal digit
+  char c;
+
+  // read each byte individually from the stream
+  while ( cin.get(c) )
+  {
+    // skip line feed characters
+    while (c == 10)
+    {
+      // get next byte from the stream
+      if ( !cin.get(c) )
+      {
+        // check if all data has been read
+        return cin.eof();
+      }
+    }
+
+    // second hexadecimal digit
+    char d = 10;
+
+    // skip line feed characters
+    while (d == 10)
+    {
+      // read second hexadecimal digit from the stream
+      if ( !cin.get(d) )
+      {
+        // notify user
+        msg::err("unexpected end of stream");
+
+        // signalize trouble
+        return false;
+      }
+    }
+
+    // the real byte value
+    unsigned char byte = 0;
+
+    // add higher byte
+    switch (c)
+    {
+      case '1':           byte +=  16; break;
+      case '2':           byte +=  32; break;
+      case '3':           byte +=  48; break;
+      case '4':           byte +=  64; break;
+      case '5':           byte +=  80; break;
+      case '6':           byte +=  96; break;
+      case '7':           byte += 112; break;
+      case '8':           byte += 128; break;
+      case '9':           byte += 144; break;
+      case 'A': case 'a': byte += 160; break;
+      case 'B': case 'b': byte += 176; break;
+      case 'C': case 'c': byte += 192; break;
+      case 'D': case 'd': byte += 208; break;
+      case 'E': case 'e': byte += 224; break;
+      case 'F': case 'f': byte += 240; break;
+    }
+
+    // add lower byte
+    switch (d)
+    {
+      case '1':           byte +=  1; break;
+      case '2':           byte +=  2; break;
+      case '3':           byte +=  3; break;
+      case '4':           byte +=  4; break;
+      case '5':           byte +=  5; break;
+      case '6':           byte +=  6; break;
+      case '7':           byte +=  7; break;
+      case '8':           byte +=  8; break;
+      case '9':           byte +=  9; break;
+      case 'A': case 'a': byte += 10; break;
+      case 'B': case 'b': byte += 11; break;
+      case 'C': case 'c': byte += 12; break;
+      case 'D': case 'd': byte += 13; break;
+      case 'E': case 'e': byte += 14; break;
+      case 'F': case 'f': byte += 15; break;
+    }
+
+    // print native byte
+    cout << byte;
+  }
+
+  // check if all data has been read
+  return cin.eof();
+}
 
 // ----
 // main
@@ -67,26 +217,7 @@
  */
 int main(int argc, char** argv)
 {
-  // check number of arguments
-  if (argc < 2)
-  {
-    // warning
-    msg::wrn( msg::insq("no arguments passed (~ passed by default)", argv[0]) );
-  }
-
-  else
-  {
-    // info: number of user-passed arguments
-    msg::nfo( msg::ins("you passed ~ argument(s)", msg::str(argc - 1)) );
-
-    // show all user-passed arguments
-    for(int i = 1; i < argc; i++)
-    {
-      // info: user-passed argument
-      msg::nfo( msg::catq("you passed: ", argv[i]) );
-    }
-  }
-
-  // signalize success
-  return 0;
+  // hexify(40);
+  unhexify();
 }
+
